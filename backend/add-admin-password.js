18736664c31229846b445aef62036591c15bd28b
Jean Encarnacion <jeanencarnacion883@gmail.com>
2026-03-27 07:58:13 +0800

    backend fully working

--- backend/add-admin-password.js
+++ backend/add-admin-password.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const { db, COLLECTIONS } = require('./config/firebase');
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 async function addAdminPassword() {
   try {
